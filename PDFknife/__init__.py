d4e08305a864b329cd1b1c485945a11f7591a76c
François Boulogne <devel@sciunto.org>
2020-05-25 15:07:35 +0200

    create shrink

--- PDFknife/__init__.py
+++ PDFknife/__init__.py
@@ -1,7 +1,9 @@
 __version__ = '0.1'
 
-from .split import split
-from .merge import merge
 from .A5 import A5
+from .merge import merge
 from .reverse import reverse
+from .shrink import shrink
+from .split import split
+
 from .engine import find_available_engine
