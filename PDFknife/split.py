8f1e020bc47b9de406c86a58cef0ebebd1ae518e
François Boulogne <devel@sciunto.org>
2020-05-25 15:18:04 +0200

    Fix PEP8

--- PDFknife/split.py
+++ PDFknife/split.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import argparse
 import subprocess
 import logging
 
 logger = logging.getLogger()
+
 
 def split(filename, output=None):
     """
