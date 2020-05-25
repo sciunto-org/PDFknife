fe61bbe4647ad1492d0efb30c4ece703f0dbe30b
François Boulogne <devel@sciunto.org>
2020-05-25 14:55:50 +0200

    Add A5

---
+++ PDFknife/A5.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import subprocess
+import logging
+import shutil
+
+logger = logging.getLogger()
+
+def A5(filename, output=None, *, engine='pdfjam'):
+    """
+    Merge successive pages into two joined A5 pages.
+
+    :param filename: PDF filepath
+    :param output: PDF output
+    """
+    if output is None:
+        output = 'A5-' + filename
+
+    if engine == 'pdfjam':
+        command = ['pdfjam',
+                   '--nup', '2x1', '--landscape',
+                   filename,
+                   '--outfile',
+                   output,
+                   ]
+    else:
+        raise ValueError(f'Wrong engine name: {engine}')
+
+    logger.debug(f'Executed command: {command}')
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate()
--- PDFknife/__init__.py
+++ PDFknife/__init__.py
@@ -2,4 +2,5 @@ __version__ = '0.1'
 
 from .split import split
 from .merge import merge
+from .A5 import A5
 from .engine import find_available_engine
