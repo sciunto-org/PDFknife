50884911d84705218786ee059dc3232ab064eb45
François Boulogne <devel@sciunto.org>
2020-05-26 15:34:55 +0200

    Read description from readme

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 
 import setuptools
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setuptools.setup(
     name         = 'PDFknife',
     version      = '0.2.0',
@@ -9,7 +16,9 @@ setuptools.setup(
     author       = "Francois Boulogne",
     license      = "BSD",
     author_email = "devel@sciunto.org",
-    description  = "",
+    description  = "Command line tools to manipulate PDF files",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     packages     = setuptools.find_packages(exclude=['doc', 'benchmarks']),
     scripts      = ['bin/pdfA5.py',
                     'bin/pdfmerge.py',
