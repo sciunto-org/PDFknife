967a6a50b6a61695838e4da8182bacf52a574afa
François Boulogne <devel@sciunto.org>
2020-09-26 12:55:12 +0200

    Move function in library

--- bin/pdfknife-even.py
+++ bin/pdfknife-even.py
@@ -6,22 +6,7 @@ import logging
 import PyPDF2
 
 from PDFknife import __version__
-
-
-def even_page(filename, output=None):
-
-    if output is None:
-        output = 'EVEN-' + filename
-
-    reader = PyPDF2.PdfFileReader(filename)
-    writer = PyPDF2.PdfFileWriter()
-    for i in range(reader.getNumPages()):
-        writer.addPage(reader.getPage(i))
-    if reader.getNumPages() % 2 == 1:
-        _, _, w, h = reader.getPage(0)['/MediaBox']
-        writer.addBlankPage(w, h)
-    with open(output, 'wb') as fd:
-        writer.write(fd)
+from PDFknife import even
 
 
 def main():
