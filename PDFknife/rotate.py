8e5ad046d796a460d9ed9245a70be326b25e5330
François Boulogne <devel@sciunto.org>
2026-07-28 06:20:07 +0200

    fix rotation

--- PDFknife/rotate.py
+++ PDFknife/rotate.py
@@ -1,25 +1,23 @@
-import subprocess
 import logging
-import shutil
+
+import pypdf
 
 from ._utils import default_output
 
 logger = logging.getLogger()
-
-ROTATIONS = {'0': 'north', '90': 'east', '180': 'south', '270': 'west'}
 
 
 def rotate(filename, angle=90, output=None):
     if output is None:
         output = default_output('ROTATED', filename)
 
-    if shutil.which('mutool'):
-        command = ['mutool', 'clean', '-R', str(angle), filename, output]
-    elif shutil.which('pdftk'):
-        direction = ROTATIONS.get(str(angle), 'east')
-        command = ['pdftk', filename, 'cat', f'1-end{direction}', 'output', output]
-    else:
-        raise RuntimeError('No engine found among mutool, pdftk.')
+    reader = pypdf.PdfReader(filename)
+    writer = pypdf.PdfWriter()
 
-    logger.debug(f'Executed command: {command}')
-    subprocess.run(command, check=True)
+    for page in reader.pages:
+        page.rotate(angle)
+        writer.add_page(page)
+        writer.add_page(page)
+
+    with open(output, 'wb') as f:
+        writer.write(f)
